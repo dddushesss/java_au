31b2a2e584a3b13f0183ae418991866cf7d14d65
dddushesss <alex1702.96pablo@yandex.ru>
2020-10-10 17:28:17 +0300

    MDs

---
+++ leetcode/src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        readWrite mdGenerator = new readWrite("leetcode/Strings.md","leetcode/leetcode_source.txt");
+        TransferToMd proceed = new TransferToMd(mdGenerator.Read('m'),mdGenerator.Read('s'));
+        mdGenerator.Write(proceed.formatToMd());
+    }
+}
