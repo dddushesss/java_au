66fc265de8b9e50fb93d4d8e54e570375d405c7e
dddushesss <alex1702.96pablo@yandex.ru>
2020-10-12 13:53:51 +0300

    Fix formatToMd

--- leetcode/src/TransferToMd.java
+++ leetcode/src/TransferToMd.java
@@ -1,4 +1,3 @@
-
 public class TransferToMd {
 
     String mdLine;
@@ -8,19 +7,19 @@ public class TransferToMd {
         this.sourceLine=sourceLine;
     }
     public String formatToMd(){
-        String[] mdSplitArray = mdLine.split("<....>\n");
+        String[] mdSplitArray = mdLine.split("<....>");
         mdSplitArray[0] = mdSplitArray[0].concat("+ [");
-        mdSplitArray[0] = mdSplitArray[0].concat(sourceLine.substring(sourceLine.indexOf(". ") + 2, sourceLine.indexOf('\n')));
+        mdSplitArray[0] = mdSplitArray[0].concat(sourceLine.substring(sourceLine.indexOf(". ") + 2, sourceLine.indexOf('\n')-1));
         mdSplitArray[0] = mdSplitArray[0].concat("](#");
-        mdSplitArray[0] = mdSplitArray[0].concat(sourceLine.substring(sourceLine.indexOf("problems/") + 9,sourceLine.indexOf("/\n")));
+        mdSplitArray[0] = mdSplitArray[0].concat(sourceLine.substring(sourceLine.indexOf("problems/") + 9,sourceLine.indexOf("/\r\n")));
         mdSplitArray[0] = mdSplitArray[0].concat(")\n");
-        mdSplitArray[0] = mdSplitArray[0].concat("<....>\n");
+        mdSplitArray[0] = mdSplitArray[0].concat("<....>");
         mdSplitArray[1] = mdSplitArray[1].concat("## ");
         mdSplitArray[1] = mdSplitArray[1].concat(sourceLine.substring(sourceLine.indexOf(". ") + 2, sourceLine.indexOf('\n')));
         mdSplitArray[1] = mdSplitArray[1].concat("\n");
-        mdSplitArray[1] = mdSplitArray[1].concat(sourceLine.substring(sourceLine.indexOf("https://"), sourceLine.indexOf("/\n") + 1));
+        mdSplitArray[1] = mdSplitArray[1].concat(sourceLine.substring(sourceLine.indexOf("https://"), sourceLine.indexOf("/\r\n") + 1));
         mdSplitArray[1] = mdSplitArray[1].concat("\n```java\n");
-        mdSplitArray[1] = mdSplitArray[1].concat(sourceLine.substring(sourceLine.indexOf("class Solution")).replaceAll("\\\s{2,}",""));
+        mdSplitArray[1] = mdSplitArray[1].concat(sourceLine.substring(sourceLine.indexOf("class Solution")));
         mdSplitArray[1] = mdSplitArray[1].concat("\n```\n");
         return mdSplitArray[0].concat(mdSplitArray[1]);
 
